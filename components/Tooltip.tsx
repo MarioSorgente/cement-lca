389ae44124d657cc399005cfe995072e582a96ce
MarioSorgente <94972242+MarioSorgente@users.noreply.github.com>
2025-08-31 14:57:53 +0200

    Create Tooltip.tsx

---
+++ components/Tooltip.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export default function Tooltip({ text }: { text: string }) {
+  const [open, setOpen] = useState(false)
+  return (
+    <span
+      className="tooltip-wrapper"
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+    >
+      <span className="tooltip-icon">i</span>
+      {open && <span className="tooltip-box">{text}</span>}
+    </span>
+  )
+}
